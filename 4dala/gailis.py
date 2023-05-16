50dfc6dee29dfcb1e791d5a384b8de0c389e5e7f
Duncan McCokiner <102353961+andzelogibala@users.noreply.github.com>
2023-05-16 20:30:09 +0300

    Delete 4dala directory

--- 4dala/gailis.py
+++
@@ -1,9 +0,0 @@
-import requests
-response=requests.get('http://universities.hipolabs.com/search?country=latvia')
-response=response.json()
-names=[]
-for school in response:
-    names.append(school['name'])
-names.sort()
-for name in names:
-    print(name)
